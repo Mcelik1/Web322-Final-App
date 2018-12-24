a464e17afb0b6d2c9475d39ba53ab8d3cb72c3c2
Mcelik1 <celik.mirza@gmail.com>
2018-12-24 00:39:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-/*********************************************************************************
-*  WEB322 – Assignment 06
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
-*  assignment has been copied manually or electronically from any other source (including web sites) or 
-*  distributed to other students.
-* 
-*  Name: Mirza Celik Student ID: 115988164 Date: January 3rd,2018
-*
-*  Online (Heroku) Link: https://blooming-refuge-30494.herokuapp.com
-*
-********************************************************************************/
+
 
 var express = require("express");
 var app = express();
